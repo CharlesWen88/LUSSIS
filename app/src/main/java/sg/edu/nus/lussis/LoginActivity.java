fb2b0b91252b4ddbd5920b4de586e2b58a5440ec
Charles Wen <charleswen88@gmail.com>
2019-08-02 13:37:16 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/nus/lussis/LoginActivity.java
@@ -0,0 +1,135 @@
+package sg.edu.nus.lussis1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class LoginActivity extends AppCompatActivity {
+//class written by Charles
+
+    EditText etUsername, etPassword;
+    Button btnLogin;
+    private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
+
+    final String url_Login = "http://10.0.2.2:56287/api/mobilelogin";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.username);
+        etPassword = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                view.startAnimation(buttonClick);
+
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                new LoginUser().execute(username, password);
+            }
+        });
+
+    }
+
+    public class LoginUser extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... strings) {
+            String username = strings[0];
+            String password = strings[1];
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+
+            //adds username and password to the formbody
+            RequestBody formBody = new FormBody.Builder()
+                    .add("Username", username)
+                    .add("Password", password)
+                    .build();
+
+            //posts the requests
+            Request request = new Request.Builder()
+                    .url(url_Login)
+                    .post(formBody)
+                    .build();
+
+            Response response = null;
+
+            //executes the response and receives the response creates a JSON object from the
+            //response string and uses the roleId to generate the next Activity page
+            try{
+                response = okHttpClient.newCall(request).execute();
+                if(response.isSuccessful() ){
+                    String result = response.body().string();
+                    if(result != null && !result.equalsIgnoreCase("null")){
+                        JSONObject jsonObj = new JSONObject(result);
+                        int role = jsonObj.getInt("RoleId");
+                        Intent i;
+                        switch (role){
+                            case 1:
+                            case 4:
+                                i = new Intent(LoginActivity.this, SecondActivity.class);
+                                startActivity(i);
+                                break;
+                            case 2:
+                                i = new Intent(LoginActivity.this, SecondActivity.class);
+                                startActivity(i);
+                                break;
+                            case 3:
+                                i = new Intent(LoginActivity.this, SecondActivity.class);
+                                startActivity(i);
+                                break;
+                            case 5:
+                                i = new Intent(LoginActivity.this, SecondActivity.class);
+                                startActivity(i);
+                                break;
+                            case 6:
+                                i = new Intent(LoginActivity.this, SecondActivity.class);
+                                startActivity(i);
+                                break;
+                            default:
+                                i = new Intent(LoginActivity.this, SecondActivity.class);
+                                startActivity(i);
+                                break;
+                        }
+                        finish();
+
+                    }else{
+                        openLoginErrorDialog();
+
+//                        Toast.makeText(LoginActivity.this,
+//                                "Wrong UserName or Password",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+    }
+
+    private void openLoginErrorDialog() {
+        LoginErrorDialog loginErrorDialog = new LoginErrorDialog();
+        loginErrorDialog.show(getSupportFragmentManager(), "dialog");
+    }
+}
---
+++ app/src/main/java/sg/edu/nus/lussis/LoginErrorDialog.java
@@ -0,0 +1,26 @@
+package sg.edu.nus.lussis1;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class LoginErrorDialog extends AppCompatDialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Error")
+                .setMessage("You have entered an incorrect User Name or Password")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+    return builder.create();
+    }
+}
